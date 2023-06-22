a3cd283b3189947efa6aee691d9f30a4bbbae420
ticcat <serfgiorico@gmail.com>
2023-06-22 12:41:07 +0200

    Refactored card cover set method and added loading state

--- components/endpoint-card/endpoint-card-behavior.js
+++ components/endpoint-card/endpoint-card-behavior.js
@@ -23,19 +23,25 @@ function defineEndpointCard(html) {
             let cardContainer =
                 this.shadowRoot.getElementById("card-container");
             let cardTitle = this.shadowRoot.getElementById("card-title");
-            let cardCover = this.shadowRoot.getElementById("card-cover");
 
             if (this.cardEndpoint.isLastLevel()) {
                 this.cardEndpoint.getSprite().then((url) => {
                     cardTitle.innerHTML = this.cardEndpoint.name;
-                    cardCover.setAttribute("src", url);
+                    this.#setCardCover(url);
                     cardContainer.onclick = this.onCardClick;
                 });
             } else {
                 cardTitle.innerHTML = this.cardEndpoint.name;
-                cardCover.setAttribute("src", this.cardEndpoint.spriteUrl);
+                this.#setCardCover(this.cardEndpoint.spriteUrl);
                 cardContainer.onclick = this.onCardClick;
             }
+        }
+
+        #setCardCover(url) {
+            let cardCover = this.shadowRoot.getElementById("card-cover");
+
+            cardCover.setAttribute("src", url);
+            cardCover.classList = ["loaded"];
         }
     }
 
