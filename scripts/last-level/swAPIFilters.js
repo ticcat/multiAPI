0182e74c367076ff41db4948e5a28d5cf6a38912
ticcat <serfgiorico@gmail.com>
2023-06-29 13:33:38 +0200

    SWAPI Films last screen implementation

---
+++ scripts/last-level/swAPIFilters.js
@@ -0,0 +1,43 @@
+export default function swAPIDataFilter(rawData, endpointType) {
+    switch (endpointType) {
+        case "Films":
+            return filterFilmData(rawData);
+        case "People":
+            console.log(2);
+            break;
+        case "Planets":
+            console.log(3);
+            break;
+        case "Species":
+            console.log(4);
+            break;
+        case "Starships":
+            console.log(5);
+            break;
+        case "Vehicles":
+            console.log(6);
+            break;
+    }
+}
+
+async function filterFilmData(rawData) {
+    const filmDataModel = {
+        episode_id: { key: "Episode Id", value: 0 },
+        opening_crawl: { key: "Opening crawl", value: "" },
+        director: { key: "Director", value: "" },
+        producer: { key: "Producer", value: "" },
+        release_date: { key: "Release date", value: "" },
+    };
+
+    let filteredData = filmDataModel;
+
+    for (let key in filmDataModel) {
+        switch (key) {
+            default: {
+                filteredData[key].value = rawData[key];
+            }
+        }
+    }
+
+    return filteredData;
+}
